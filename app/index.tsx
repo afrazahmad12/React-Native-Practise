d3c6bf4379b806469987626eafb2f4d881a0f414
Afraz Ahmad <afrazbaig27@gmail.com>
2025-07-22 14:43:04 +0500

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+const Index = () => {
+  return (
+    <View
+      style={{
+        backgroundColor: "red",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+      <Text style={{ fontSize: 20, fontWeight: "700", color: "white" }}>
+        Hello
+      </Text>
+
+    </View>
+  );
+}
+export default Index;
