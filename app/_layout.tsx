c9ea4f79d0bee35ee967d0dc15c41c90674128f7
Afraz Ahmad <afrazbaig27@gmail.com>
2025-07-24 13:27:10 +0500

    Initial Route Issue Fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,20 +5,13 @@ import React from "react";
 
 const RootLayout = () => {
   return (
-    <Stack>
+    <Stack initialRouteName="flexlayout" screenOptions={{ headerShown: false }}>
       {/* This line is enable for drawer and bottom tab navigation */}
       {/* <Stack.Screen name="(drawer)"  options={{ headerShown: false }} /> */}
-      <Stack initialRouteName="login" screenOptions={{ headerShown: false }} />
-      <Stack.Screen name="flexlayout" />
-     
       <Stack.Screen name="login" />
+      <Stack.Screen name="flexlayout" />
       <Stack.Screen name="signup" />
-       <Stack.Screen name="flexBox" />
-
-
-
-
-
+      <Stack.Screen name="flexBox" />
     </Stack>
   )
 }
